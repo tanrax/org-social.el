5e888e3049c52dfe38fdc6e60dfb6ebca9fcbe45
Andros Fenollosa <andros@fenollosa.email>
2025-09-30 16:10:51 +0200

    Updated version

--- org-social-file.el
+++ org-social-file.el
@@ -3,7 +3,7 @@
 ;; SPDX-License-Identifier: GPL-3.0
 
 ;; Author: Andros Fenollosa <hi@andros.dev>
-;; Version: 1.6
+;; Version: 2.0
 ;; URL: https://github.com/tanrax/org-social.el
 ;; Package-Requires: ((emacs "30.1") (org "9.0") (request "0.3.0") (seq "2.20") (cl-lib "0.5") (emojify "1.2"))
 
--- org-social-relay.el
+++ org-social-relay.el
@@ -3,7 +3,7 @@
 ;; SPDX-License-Identifier: GPL-3.0
 
 ;; Author: Andros Fenollosa <hi@andros.dev>
-;; Version: 1.6
+;; Version: 2.0
 ;; Package-Requires: ((emacs "30.1") (request "0.3.0") (json "1.4"))
 
 ;;; Commentary:
